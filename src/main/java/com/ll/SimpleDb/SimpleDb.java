9dfc4fbf91c56c7cd40c274ebbc010d51cfb320e
whqtker <chosh2001@naver.com>
2025-01-11 15:58:11 +0900

    feat: DB 연결하는 SimpleDb 생성자 로직 구현
    테스트 X

--- src/main/java/com/ll/SimpleDb/SimpleDb.java
+++ src/main/java/com/ll/SimpleDb/SimpleDb.java
@@ -1,8 +1,23 @@
 package com.ll.SimpleDb;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+
 public class SimpleDb {
     private final Sql sql = new Sql();
+    Connection conn = null;
+    PreparedStatement prst = null;
     public SimpleDb(String hostName, String userName, String password, String dbName) {
+        String url = "jdbc:mysql://" + hostName + "/" + dbName;
+
+        try {
+            // MySQL JDBC Driver 로드
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conn = DriverManager.getConnection(url, userName, password); // DB 연결
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
     public void setDevMode(boolean b) {
     }
